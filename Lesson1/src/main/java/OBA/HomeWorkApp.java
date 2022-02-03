26ffff0c126cd282503e4540ea10c83d7faebf02
Vladislav <monodemonzzz@mail.ru>
2022-02-04 00:30:42 +0300

    First Task

--- Lesson1/src/main/java/OBA/HomeWorkApp.java
+++
@@ -1,9 +0,0 @@
-package OBA;
-
-public class HomeWorkApp {
-    public static void printThreeWords(){
-        System.out.println("Orange");
-        System.out.print("Banana");
-        System.out.print("Apple");
-    }
-}
