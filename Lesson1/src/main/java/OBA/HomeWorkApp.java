12dc7ad6768e8c2fdc8df6df24278f452ac8fbc2
Vladislav <monodemonzzz@mail.ru>
2022-02-03 23:12:08 +0300

    Hi

---
+++ Lesson1/src/main/java/OBA/HomeWorkApp.java
@@ -0,0 +1,9 @@
+package OBA;
+
+public class HomeWorkApp {
+    public static void printThreeWords(){
+        System.out.println("Orange");
+        System.out.print("Banana");
+        System.out.print("Apple");
+    }
+}
--- Lesson1/src/main/java/lesson1/MainApp.java
+++ Lesson1/src/main/java/lesson1/MainApp.java
@@ -1,5 +1,8 @@
 package lesson1;
 
 public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("Orange Banana Apple");
 
+    }
 }
